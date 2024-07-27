0d1357826669a4e3c86ec2467a1553c713bc0c10
Declan Riedel <declan.riedel@protonmail.com>
2024-07-27 15:52:42 +0200

    colors exist now

--- misc/config.nix
+++ misc/config.nix
@@ -23,8 +23,27 @@
 
   stylix.enable = true;
   stylix.autoEnable = true;
-  stylix.base16Scheme =
-    "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
+
+  stylix.base16Scheme = {
+    base00 = "000000"; # Pitch black
+    base01 = "333333"; # Slightly lighter black
+    base02 = "555555"; # Medium gray
+    base03 = "777777"; # Slightly darker gray
+    base04 = "bbbbbb"; # Light gray
+    base05 = "dddddd"; # Slightly lighter gray
+    base06 = "bbbbbb"; # Light gray
+    base07 = "ffffff"; # White
+    base08 = "ff6666"; # Vibrant red
+    base09 = "ffaa33"; # Vibrant orange
+    base0A = "ffaa33"; # Vibrant orange
+    base0B = "99ff33"; # Vibrant green
+    base0C = "33ffff"; # Vibrant cyan
+    base0D = "33aaff"; # Vibrant blue
+    base0E = "cc66ff"; # Vibrant purple
+    base0F = "ff9999"; # Vibrant reddish-brown
+
+  };
+
   stylix.image = /home/declan/Media/Pictures/1107977.jpg;
 
   stylix.targets.nixvim = {
@@ -45,12 +64,12 @@
   stylix.polarity = "dark";
 
   #services.mpd = {
-    #enable = true;
-    #user = "declan";
-    #startWhenNeeded = true;
-    #playlistDirectory = "/home/declan/Media/Music";
-    #muiscDirectory = "/home/declan/Media/Music";
-    #};
+  #enable = true;
+  #user = "declan";
+  #startWhenNeeded = true;
+  #playlistDirectory = "/home/declan/Media/Music";
+  #muiscDirectory = "/home/declan/Media/Music";
+  #};
 
   #here because merge error that doesnt make sense when importing into default.nix????? 
   system.stateVersion = "24.05"; # Did you read the comment?
