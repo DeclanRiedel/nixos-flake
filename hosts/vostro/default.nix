e2b9d1aea86757c0a6b33a73df90fad814e0aedc
Declan Riedel <declan.riedel@protonmail.com>
2026-05-13 13:08:16 +0200

    Enable Sunshine streaming on vostro

--- hosts/vostro/default.nix
+++ hosts/vostro/default.nix
@@ -24,4 +24,18 @@
   services.openssh.extraConfig = lib.mkAfter ''
     PermitRootLogin yes
   '';
+  boot.kernelModules = [ "uinput" ];
+
+  services.udev.extraRules = ''
+    KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
+  '';
+
+  services.sunshine = {
+    enable = true;
+    openFirewall = true;
+    capSysAdmin = true;
+    settings = {
+      capture = "kms";
+    };
+  };
 }
