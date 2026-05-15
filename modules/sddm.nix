853370ce20ffe778204de5f9348ae6a596784710
Declan Riedel <declan.riedel@protonmail.com>
2026-05-15 10:32:18 +0200

    fix: stabilize sddm login theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,73 +1,52 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs;
-    [
-      (sddm-chili-theme.override {
-        themeConfig = {
-          ScreenWidth = 1920;
-          ScreenHeight = 1080;
-          blur = true;
-          recursiveBlurLoops = 5;
-          recursiveBlurRadius = 5;
-          background = ../wall/sddm-wall.jpg;
-        };
-      })
-    ];
+{ pkgs, ... }:
 
-  environment.etc."wayland-sessions/Hyprland.desktop".text = ''
-    [Desktop Entry]
-    Name=Hyprland
-    Comment=Tiling wayland compositor
-    Exec=Hyprland
-    Type=Application
-    Keywords=wm;tiling
-  '';
+{
+  environment.systemPackages = [
+    (pkgs.sddm-chili-theme.override {
+      themeConfig = {
+        ScreenWidth = 1920;
+        ScreenHeight = 1080;
+        blur = false;
+        recursiveBlurLoops = 5;
+        recursiveBlurRadius = 5;
+        background = ../wall/sddm-wall.jpg;
+      };
+    })
+  ];
 
   services.displayManager = {
-    # startx.enable = false;
+    defaultSession = "hyprland";
 
     sddm = {
       enable = true;
-      settings = {
-        #Theme = {
-        #  FacesDir =
-        #    "/home/declan/.dotfiles/nixos-flake/wall/faces"; # what the fuck??
-        #};
-      };
-      wayland.enable = true;
+      wayland.enable = false;
       theme = "chili";
     };
   };
 
-  # Define systemd service to run on boot
-  systemd.services."sddm-avatar" = {
-    description = "Service to copy or update users Avatars at startup.";
-    wantedBy = [ "multi-user.target" ];
-    before = [ "sddm.service" ];
-    script = ''
-      for user in /home/*; do
+  system.activationScripts.sddm-avatar.text = ''
+    install -d -m 0755 /var/lib/AccountsService/icons
 
-        username=$(basename "$user")
-        icon_source="$user/.face.icon"
-        icon_dest="/var/lib/AccountsService/icons/$username"
+    for user in /home/*; do
+      username=$(basename "$user")
+      icon_source="$user/.face.icon"
+      icon_dest="/var/lib/AccountsService/icons/$username"
 
-        if [ -f "$icon_source" ]; then
-          if [ ! -f "$icon_dest" ] || ! cmp -s "$icon_source" "$icon_dest"; then
-            rm -f "$icon_dest"
-            cp -L "$icon_source" "$icon_dest"
-          fi
+      if [ -f "$icon_source" ]; then
+        if [ ! -f "$icon_dest" ] || ! cmp -s "$icon_source" "$icon_dest"; then
+          rm -f "$icon_dest"
+          cp -L "$icon_source" "$icon_dest"
+          chmod 0644 "$icon_dest"
         fi
+      fi
+    done
+  '';
 
-      done
-    '';
-    serviceConfig = {
-      Type = "simple";
-      User = "root";
-      StandardOutput = "journal+console";
-      StandardError = "journal+console";
-    };
+  systemd.tmpfiles.rules = [
+    "d /var/lib/AccountsService/icons 0755 root root -"
+  ];
+
+  systemd.services.sddm = {
+    after = [ "systemd-tmpfiles-setup.service" ];
   };
-
-  # Ensures SDDM starts after the service.
-  systemd.services.sddm = { after = [ "sddm-avatar.service" ]; };
-
 }
