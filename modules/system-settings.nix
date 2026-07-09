55bf858cdff3f634b451ecd413ba088b95ffdf4e
Declan Riedel <declan.riedel@protonmail.com>
2026-07-09 17:21:11 +0200

    Configure nix gc, optimiser, and binary caches

--- modules/system-settings.nix
+++ modules/system-settings.nix
@@ -55,9 +55,35 @@
   ## touchpad support? hyprland does it already?
   services.libinput.enable = true;
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [ "weekly" ];
+    };
+
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "declan" ];
+      auto-optimise-store = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      ];
+    };
   };
 
   systemd.services.nix-gc.preStart = ''
